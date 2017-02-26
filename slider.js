f258a706299fdc96908fc1ef4d4da336230aa863
AnaStylinson <anagzzhdz@hotmail.com>
2017-02-25 22:08:46 -0600

    Slider 2
    Slider 2

--- slider.js
+++ slider.js
@@ -5,19 +5,44 @@ var siguiente = $('#btn-next');
 var anterior = $('#btn-prev');
 
 //mover ultima imagen al primer lugar
-$('#slider section:last').insertBefore('#slider section:first');
-//mostrar la primer imagen con un margen de -100%
+$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+//mostrar la primera imagen con un margen de -100%
 slider.css('margin-left', '-'+100+'%');
 
-function moverD(){
-    slider.animate({
-        marginLeft:'-'+200+'%'
-    }   ,700, function(){
-        
-    });
+function moverD() {
+	slider.animate({
+		marginLeft:'-'+200+'%'
+	} ,700, function(){
+		$('#slider .slider__section:first').insertAfter('#slider .slider__section:last');
+		slider.css('margin-left', '-'+100+'%');
+	});
 }
 
-siguiente.on('click', function(){
-             moverD();
+function moverI() {
+	slider.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+function autoplay() {
+	interval = setInterval(function(){
+		moverD();
+	}, 5000);
+}
+siguiente.on('click',function() {
+	moverD();
+	clearInterval(interval);
+	autoplay();
 });
 
+anterior.on('click',function() {
+	moverI();
+	clearInterval(interval);
+	autoplay();
+});
+
+
+autoplay();
